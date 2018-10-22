8e933b3705f748fcbe0bed724983ab1a019bdcb3
Ronald Young <ronnie.kingsley.young@gmail.com>
2018-10-22 19:17:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,18 +4,33 @@ function getCart() {
  return cart;
 }
 
-function setCart(c) {
-  cart = c;
+function setCart(newC) {
+  cart = newC;
   return cart;
 }
 
 function addToCart(item) {
- // write your code here
+  var price = Math.floor(Math.random()*100 + 1)
+  var nameItem = {}
+  nameItem[item] = price
+  cart.push(nameItem) 
+  console.log(`${item} has been added to your cart.`)
+  return cart
+  
+ 
+ 
 }
 
 function viewCart() {
-  // write your code here
-}
+  var items = []
+  if (cart.length === 0){
+    console.log ('Your shopping cart is empty.')
+    for (var i = 0, i < cart.length, i++)
+    
+   }else if (cart.length === 1)
+   var oneItem = `In your cart, you have ${cart[0]} at $ ${cart[0]}.`
+   console.log (oneItem) }
+
 
 function total() {
   // write your code here
